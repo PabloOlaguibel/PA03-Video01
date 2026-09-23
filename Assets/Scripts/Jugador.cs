106300512d36049dbab5956238c77f45ea0a30c6
PabloOlaguibel <74240945@continental.edu.pe>
2026-09-22 19:17:08 -0500

    Configuración de Jugador
    Configuración de Jugador

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-    
+    public Rigidbody rigidbody;
+
+
     void Start()
     {
+        rigidbody.useGravity = false;
+
         Debug.Log("Hola Mundo");
 
 
