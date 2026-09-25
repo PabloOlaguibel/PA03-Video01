2ceec1f1810e913dfe55e1fe2e4c330b7acc3caf
PabloOlaguibel <74240945@continental.edu.pe>
2026-09-24 21:46:30 -0500

    Configuracion de enemigo
    Configuracion de enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
