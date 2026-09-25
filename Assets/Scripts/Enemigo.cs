2b96a4106184d138cf851a1c12ded59e43ca0ad2
PabloOlaguibel <74240945@continental.edu.pe>
2026-09-24 21:52:48 -0500

    Enemigo Matandome al colisionar
    Enemigo Matandome al colisionar

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,19 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Enemigo : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Transform playerTransform;
 
-    void Start()
+    private void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
 
+        GameObject jugador = GameObject.FindGameObjectWithTag("Player");
+
+        if (jugador != null)
+        {
+            playerTransform = jugador.transform;
+        }
     }
 
-    void Update()
+    private void Update()
     {
+        if (playerTransform != null)
+        {
+            navMeshAgent.destination = playerTransform.position;
+        }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().buildIndex
+            );
+        }
     }
 }
